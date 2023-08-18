02bb768b1ac427fcc04c9f6fcbb1af88e93e1356
jokesh06 <ankeshwarapujokesh06@gmail.com>
2023-08-18 16:39:31 +0530

    third commit

---
+++ boolean.java
@@ -0,0 +1,17 @@
+class ClassA
+{
+public static void main(String[] args)
+{
+   int a=15; int b=25;
+     a=a+b;
+     b=a-b;
+     a=a-b;
+
+{
+       System.out.println(a);
+      System.out.println(b);
+     
+}
+
+}
+}
