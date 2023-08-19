0b85c0c66efa34ec702c41e4321c2eb7459004b0
jokesh06 <ankeshwarapujokesh06@gmail.com>
2023-08-19 15:38:02 +0530

    Basic Program

---
+++ fizz1.java
@@ -0,0 +1,17 @@
+class ClassA
+{
+public static void main(String[] args)
+{
+ int x=25;
+    if (x%3==0) {
+    System.out.println ("Good morning");
+    } else (X%5==0) {
+     System.out.println ("Good day");
+     }
+ else 
+{
+     System.out.println("Good evening");
+     }
+
+  }  
+}
